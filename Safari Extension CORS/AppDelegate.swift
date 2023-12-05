0d52bb8b0de28a0042cf0e1dcf48e95f26f8ed5f
Paulo Orquillo <olopsman@users.noreply.github.com>
2023-12-05 15:56:17 +1300

    Initial Commit

---
+++ Safari Extension CORS/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  Safari Extension CORS
+//
+//  Created by Paulo Orquillo on 5/12/23.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    func applicationDidFinishLaunching(_ notification: Notification) {
+        // Override point for customization after application launch.
+    }
+
+    func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+        return true
+    }
+
+}
